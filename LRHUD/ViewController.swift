87af0240097191c7b7184131838fa0babb2fb775
刘彦直 <karlcool.l@qq.com>
2021-12-10 11:39:17 +0800

    Initial Commit

---
+++ LRHUD/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LRHUD
+//
+//  Created by 刘彦直 on 2021/12/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
